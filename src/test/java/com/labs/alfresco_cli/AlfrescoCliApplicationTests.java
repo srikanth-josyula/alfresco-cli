c5deb4eee011d0fcb91fd42fb1e56afdd28d4f1f
Srikanth Josyula <srikanthjosyula@Srikanths-Air.hitronhub.home>
2024-08-29 13:02:56 -0300

    initial commit

---
+++ src/test/java/com/labs/alfresco_cli/AlfrescoCliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.labs.alfresco_cli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlfrescoCliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
